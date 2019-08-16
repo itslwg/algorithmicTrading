437d077b91fef9d3c47c50e70fbf7aab49480efc
Ludde <ludviggerdin@gmail.com>
2019-08-16 16:46:14 +0200

    Add package files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'algorithmicTrading',
+      version = '0.1',
+      description = '',
+      url = 'To be inserted',
+      author = 'itslwg',
+      author_email = 'ludviggerdin@gmail.com',
+      licence = 'MIT',
+      packages = ['algorithmicTrading'],
+      zip_safe = False)
